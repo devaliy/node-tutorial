d50f8a7423c67e4fc188d7c044cc2b7c88016135
Aliu Adedigba <indexcorpng@gmail.com>
2024-06-12 04:14:24 +0100

    create  and writting file

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const {
-    obj,func
-} = require('./utils')
-console.log(obj)
-console.log(func(3, 4))
-console.log(__dirname, __filename)
+const fs = require('fs');
+fs.writeFileSync('app.txt', 'Hi Welcome to the node js course')
+
+
+
+// const {
+//     obj,func
+// } = require('./utils')
+// console.log(obj)
+// console.log(func(3, 4))
+// console.log(__dirname, __filename)
